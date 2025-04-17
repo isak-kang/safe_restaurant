d46b92deba963b28cab51a087253d082c7a0d535
isak-kang <wbsldj59@naver.com>
2025-04-18 00:01:31 +0900

    test중인 자료

--- backend/test.py
+++ backend/test.py
@@ -1,60 +1,225 @@
-import requests
-import json
-import pandas as pd
-import csv
-import pymysql
-import os
-from sqlalchemy import create_engine
-from tqdm import tqdm
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from bs4 import BeautifulSoup
 import time
-import logging
+import pandas as pd
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support import expected_conditions as EC
+import sys
+import os
+from DB import df_load
+from tqdm import tqdm
 
-from dotenv import load_dotenv
-load_dotenv()
+# Selenium 설정
+chrome_options = Options()
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36")
+chrome_options.add_argument("--log-level=3")
 
-from DataBase.DB import df_load,df_save
+# ... 기존 import 및 설정 생략 ...
 
-from datetime import datetime
+def crawling_img_url():
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+    wait = WebDriverWait(driver, 10)
 
+    df = df_load("""
+        SELECT UPSO_NM, SITE_ADDR_RD
+        FROM model_restaurant_apply
+        WHERE ASGN_YMD != '' AND ASGN_CANCEL_YMD = ''
+        ORDER BY ASGN_YMD DESC;
+    """)
+    df['SITE_ADDR_RD'] = df['SITE_ADDR_RD'].str.split(',').str[0]
+    df["img_search_addr"] = df["SITE_ADDR_RD"].fillna('') + "," + df["UPSO_NM"].fillna('')
 
-	
-# DB
-user = os.environ.get('MYSQL_USER')
-password = os.environ.get('MYSQL_PASSWORD')
-host = os.environ.get('MYSQL_HOST')
-db = os.environ.get('MYSQL_DB')
-engine = create_engine(f"mysql+pymysql://{user}:{password}@{host}/{db}")
+    result = []
+    a= 0
+    for idx, row in tqdm(df.iterrows(), total=len(df)):
+        a+=1
+        search_addr = row["img_search_addr"]
+        url = "https://map.kakao.com/"
+        driver.get(url)
 
-gocode = os.environ.get('GOCODE_KEY')
+        try:
+            search_area = driver.find_element(By.XPATH, r'//*[@id="search.keyword.query"]')
+            search_area.clear()
+            search_area.send_keys(search_addr)
+            driver.find_element(By.XPATH, r'//*[@id="search.keyword.submit"]').send_keys(Keys.ENTER)
 
-# 주소 -> 좌표 
-def get_coords(address):
-    apiurl = "https://api.vworld.kr/req/address?"
-    params = {
-        "service": "address",
-        "request": "getcoord",
-        "crs": "epsg:4326",
-        "address": address,
-        "format": "json",
-        "type": "road",
-        "key": gocode
-    }
+            time.sleep(1)
 
-    try:
-        response = requests.get(apiurl, params=params, timeout=3)
-        if response.status_code == 200:
-            result = response.json().get("response", {}).get("result")
-            if result and "point" in result:
-                point = result["point"]
-                return address,":",float(point["x"]), float(point["y"])
-                
+            html = driver.page_source
+            soup = BeautifulSoup(html, 'html.parser')
+            first_place = soup.select_one('.placelist > .PlaceItem')
+
+            if first_place:
+                score_tag = first_place.select_one('.rating > .score > em')
+                link_tag = first_place.select_one('a.numberofscore')
+
+                score = score_tag.text if score_tag else 'N/A'
+                detail_url = link_tag.get('href') if link_tag else '링크 없음'
+                detail_url = detail_url.split("#")[0] if detail_url != '링크 없음' else detail_url
+
+                img_urls = []
+
+                if detail_url != '링크 없음':
+                    driver.get(detail_url)
+                    time.sleep(1)
+
+                    detail_html = driver.page_source
+                    detail_soup = BeautifulSoup(detail_html, 'html.parser')
+
+                    img_tags = detail_soup.find_all('img', class_='img-thumb')
+
+                    for img in img_tags:
+                        img_url = img.get('src')
+                        if img_url:
+                            img_url = "https:" + img_url if img_url.startswith("//") else img_url
+                            img_urls.append(img_url)
+
+                result.append({
+                    "img_search_addr": search_addr,
+                    "score": score,
+                    "img_urls": "; ".join(img_urls) if img_urls else '없음'
+                })
+                print({
+                    "img_search_addr": search_addr,
+                    "score": score,
+                    "img_urls": "; ".join(img_urls) if img_urls else '없음'
+                })
             else:
-                logging.warning(f"[결과 없음] 주소: {address}")
-        else:
-            logging.warning(f"[응답 오류] 주소: {address}, Status Code: {response.status_code}")
-    except Exception as e:
-        logging.warning(f"[예외 발생] 주소: {address}, 예외: {str(e)}")
+                result.append({
+                    "img_search_addr": search_addr,
+                    "score": "장소 없음",
+                    "img_urls": "없음"
+                })
+                print({
+                    "img_search_addr": search_addr,
+                    "score": "장소 없음",
+                    "img_urls": "없음"
+                })
 
-    return None, None
+        except Exception as e:
+            print(f"[ERROR] {search_addr} 처리 중 오류 발생: {e}")
+            result.append({
+                "img_search_addr": search_addr,
+                "score": "오류",
+                "img_urls": "없음"
+            })
 
-print(get_coords("서울특별시 중랑구 중랑천로10길 39"))
+    driver.quit()
+
+    # 결과 저장
+    result_df = pd.DataFrame(result)
+    result_df.to_csv("crawled_kakao_img_urls.csv", index=False, encoding='utf-8-sig')
+    print("✅ CSV 저장 완료: crawled_kakao_img_urls.csv")
+
+
+
+
+
+ 
+
+def crawling_img_url_save():
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+    wait = WebDriverWait(driver, 10)
+
+    df = df_load("""
+        SELECT UPSO_NM, SITE_ADDR_RD
+        FROM model_restaurant_apply
+        WHERE ASGN_YMD != '' AND ASGN_CANCEL_YMD = ''
+        ORDER BY ASGN_YMD DESC;
+    """)
+    df['SITE_ADDR_RD'] = df['SITE_ADDR_RD'].str.split(',').str[0]
+    df["img_search_addr"] = df["SITE_ADDR_RD"].fillna('') + "," + df["UPSO_NM"].fillna('')
+
+    result = []
+    a= 0
+    for idx, row in tqdm(df.iterrows(), total=len(df)):
+        a+=1
+        search_addr = row["img_search_addr"]
+        url = "https://map.kakao.com/"
+        driver.get(url)
+
+        try:
+            search_area = driver.find_element(By.XPATH, r'//*[@id="search.keyword.query"]')
+            search_area.clear()
+            search_area.send_keys(search_addr)
+            driver.find_element(By.XPATH, r'//*[@id="search.keyword.submit"]').send_keys(Keys.ENTER)
+
+            time.sleep(1)
+
+            html = driver.page_source
+            soup = BeautifulSoup(html, 'html.parser')
+            first_place = soup.select_one('.placelist > .PlaceItem')
+
+            if first_place:
+                score_tag = first_place.select_one('.rating > .score > em')
+                link_tag = first_place.select_one('a.numberofscore')
+
+                score = score_tag.text if score_tag else 'N/A'
+                detail_url = link_tag.get('href') if link_tag else '링크 없음'
+                detail_url = detail_url.split("#")[0] if detail_url != '링크 없음' else detail_url
+
+
+                result.append({
+                    "img_search_addr": search_addr,
+                    "score": score,
+                    "img_url": detail_url
+                })
+                print({
+                    "img_search_addr": search_addr,
+                    "score": score,
+                    "img_url": detail_url
+                })
+            else:
+                result.append({
+                    "img_search_addr": search_addr,
+                    "score": "장소 없음",
+                    "img_url": "없음"
+                })
+                print({
+                    "img_search_addr": search_addr,
+                    "score": "장소 없음",
+                    "img_url": "없음"
+                })
+
+        except Exception as e:
+            print(f"[ERROR] {search_addr} 처리 중 오류 발생: {e}")
+            result.append({
+                "img_search_addr": search_addr,
+                "score": "오류",
+                "img_urls": "없음"
+            })
+
+    driver.quit()
+
+    # 결과 저장
+    result_df = pd.DataFrame(result)
+    result_df.to_csv("crawled_kakao_img_url.csv", index=False, encoding='utf-8-sig')
+    print("✅ CSV 저장 완료: crawled_kakao_img_url.csv")
+
+
+
+
+
+
+
+
+
+
+
+
+
+# 실행
+
+if __name__ == "__main__":
+
+    # crawling_img_url()
+    crawling_img_url_save()
+    pass
