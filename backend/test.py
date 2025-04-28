f2aef3d03163fcd770625e2b155e061c7068bf14
isak-kang <wbsldj59@naver.com>
2025-04-29 02:08:20 +0900

    test

--- backend/test.py
+++ backend/test.py
@@ -1,8 +1,7 @@
 
-import requests
 
 from DataBase.DB import df_load
-
+import requests
 def test():
     # 음식점 정보 조회
     query = """
@@ -11,10 +10,12 @@ def test():
             SNT_UPTAE_NM
         FROM model_restaurant_apply
         WHERE ASGN_YMD != '' AND ASGN_CANCEL_YMD = '';
-
-            """
+    """
 
     df = df_load(query)
-
+    df["addr"] = df["SITE_ADDR_RD"].apply(lambda x: x.split(" ")[1] if isinstance(x, str) and len(x.split(" ")) > 1 else "")
+    gu_options = sorted(df["addr"].dropna().unique())
+    uptae_options = sorted(df["SNT_UPTAE_NM"].dropna().unique())
+    print(gu_options)
 
 test()
