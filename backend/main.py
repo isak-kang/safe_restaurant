f2c0f1e50b7468a91d4cbf809dc9d3d44f41c230
isak-kang <wbsldj59@naver.com>
2025-04-29 01:53:17 +0900

    즐겨찾기 및 모범식당 쿼리문 수정

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,9 @@
 from fastapi import FastAPI,Query,HTTPException, Path,Form,Depends
 from typing import List
 from fastapi.middleware.cors import CORSMiddleware
-from DataBase.DB import df_load,df_save,row_insert
+from DataBase.DB import df_load,df_save,row_insert,delete_data
 from fastapi.responses import JSONResponse
-
+import pandas as pd
 from utils.user import hash_password, verify_password,check_duplicate_id,create_access_token,verify_token
 
 import jwt
@@ -29,7 +29,7 @@ async def model_restaurant(gu: str = "", uptae: str = "", name: str = ""):
             CGG_CODE,
             ASGN_YMD,
             ASGN_YY,
-            UPSO_NM,
+            upso_nm,
             SITE_ADDR_RD,
             SNT_UPTAE_NM,
             TRDP_AREA,
@@ -41,33 +41,39 @@ async def model_restaurant(gu: str = "", uptae: str = "", name: str = ""):
     df = df_load(query)
 
     # 이미지 정보 조회
-    query2 = "SELECT * FROM restaurant_hygiene.restaurant_images;"
+    query2 = "SELECT A.upso_nm, A.img_urls, B.score, A.addr FROM restaurant_hygiene.restaurant_images A join kakao B on A.id = B.id;"
     df_photo = df_load(query2)
-
+    # print(df_photo)
     # 이미지 이름 정제 및 추출
     df_photo["img_url"] = df_photo["img_urls"].apply(lambda x: x.split(";")[0].strip())
 
     # 음식점 이름 정제 후 조인 기준 열 생성
-    df["normalized_upso"] = df["UPSO_NM"].str.replace(" ", "").str.lower()
-    df_photo["normalized_upso"] = df_photo["upso_name"].str.replace(" ", "").str.lower()
+    df["normalized_upso"] = df["upso_nm"].str.replace(" ", "").str.lower()
+    df_photo["normalized_upso"] = df_photo["upso_nm"].str.replace(" ", "").str.lower()
 
+    df_photo['SITE_ADDR_RD'] = df_photo['addr']
+    df['SITE_ADDR_RD'] = df['SITE_ADDR_RD'].apply(lambda x: x.split(",")[0])
     # 이미지 URL 병합
-    df = df.merge(df_photo[["normalized_upso", "img_url"]], how="left", on="normalized_upso")
+    df = df.merge(
+    df_photo[["normalized_upso", "SITE_ADDR_RD", "img_url", "score"]],
+    how="left",
+    on=["normalized_upso", "SITE_ADDR_RD"]
+)
 
     # 주소 필터용 행정동 추출
     df["addr"] = df["SITE_ADDR_RD"].apply(lambda x: x.split(" ")[1] if isinstance(x, str) and len(x.split(" ")) > 1 else "")
-
+    # print(df["score"])
     # 필터 조건 적용
     if gu:
         df = df[df["addr"] == gu]
     if uptae:
         df = df[df["SNT_UPTAE_NM"] == uptae]
     if name:
-        df = df[df["UPSO_NM"].str.contains(name, case=False, na=False)]
-
-    # 응답 형식 정리
+        df = df[df["upso_nm"].str.contains(name, case=False, na=False)]
+    
+    # 응답 형식 정리   
     response = df[[
-        "UPSO_NM",
+        "upso_nm",
         "SITE_ADDR_RD",
         "SNT_UPTAE_NM",
         "TRDP_AREA",
@@ -75,9 +81,9 @@ async def model_restaurant(gu: str = "", uptae: str = "", name: str = ""):
         "ASGN_YMD",
         "img_url",
         "addr",
-        "MAIN_EDF"
+        "MAIN_EDF",
+        "score"
     ]].fillna("").to_dict(orient="records")
-
     return JSONResponse(content=response)
 
 
@@ -105,13 +111,13 @@ async def get_restaurant_by_name(upso_nm: str = Path(..., title="업소명")):
             CGG_CODE,
             ASGN_YMD,
             ASGN_YY,
-            UPSO_NM,
+            upso_nm,
             SITE_ADDR_RD,
             SNT_UPTAE_NM,
             TRDP_AREA,
             ADMDNG_NM
         FROM model_restaurant_apply
-        WHERE UPSO_NM = '{upso_nm}'
+        WHERE upso_nm = '{upso_nm}'
         AND ASGN_YMD != '' AND ASGN_CANCEL_YMD = ''
         LIMIT 1;
     """
@@ -124,14 +130,12 @@ async def get_restaurant_by_name(upso_nm: str = Path(..., title="업소명")):
 @app.get("/api/get_gocode")
 async def get_gocode(address: str):
     SITE_ADDR_RD = address.split(",")[0].strip()
-    print(SITE_ADDR_RD)
     query = f"SELECT * FROM tb_map WHERE SITE_ADDR_RD = '{SITE_ADDR_RD}'"
     df = df_load(query)
     if df.empty:
         raise HTTPException(status_code=404, detail="해당 주소에 대한 좌표 정보가 없습니다.")
 
     result = df.iloc[0].to_dict()
-    print(result)
     return {
         "LAT": result.get("latitude"),
         "LNG": result.get("longitude")
@@ -140,49 +144,60 @@ async def get_gocode(address: str):
 
 @app.get("/api/main_map")
 async def model_restaurant(gu: str = ""):
-    query1 = f"SELECT * FROM tb_map"
-    query2 = f"SELECT SITE_ADDR_RD FROM restaurant_hygiene.model_restaurant_apply;"
+    query1 = f"""SELECT 
+            A.latitude, 
+            A.longitude, 
+            A.SITE_ADDR_RD, 
+            B.upso_nm
+        FROM 
+            tb_map A 
+        JOIN (
+            SELECT DISTINCT 
+                SUBSTRING_INDEX(SITE_ADDR_RD, ',', 1) AS clean_addr, 
+                upso_nm
+            FROM 
+                model_restaurant_apply
+            where ASGN_YMD != '' AND ASGN_CANCEL_YMD = ''
+        ) B
+        ON 
+            A.SITE_ADDR_RD = B.clean_addr;"""
     map_df = df_load(query1)
-    model_addr_df = df_load(query2)
-
-    model_addr_df["addr"] = model_addr_df["SITE_ADDR_RD"].apply(lambda x: x.split(",")[0])
-
-    # model_addr_df에서 'addr' 값에 해당하는 주소만 필터링한 후 복사본으로 만듭니다.
-    filtered_df = map_df[map_df['SITE_ADDR_RD'].isin(model_addr_df['addr'])].copy()
 
     # 필터링된 주소에서 '구' 정보를 추출하여 addr_gu 컬럼으로 추가합니다.
-    filtered_df["addr_gu"] = filtered_df["SITE_ADDR_RD"].apply(
+    map_df["addr_gu"] = map_df["SITE_ADDR_RD"].apply(
         lambda x: x.split(" ")[1] if isinstance(x, str) and len(x.split(" ")) > 1 else ""
     )
-  
-
+    
     if gu:
-        filtered_df = filtered_df[filtered_df["addr_gu"] == gu]
-    return JSONResponse(content=filtered_df.to_dict(orient="records"))
+        map_df = map_df[map_df["addr_gu"] == gu]
+
+
+    return JSONResponse(content=map_df.to_dict(orient="records"))
 
 
 @app.get("/api/restaurant_photo")
 async def restaurant_photo(upso: str = ""):
-    query = """SELECT DISTINCT A.upso_name, A.img_urls, B.score, B.url, B.addr
+    query = """SELECT DISTINCT A.upso_nm, A.img_urls, B.score, B.url, B.addr
             FROM (
-                SELECT upso_name, MAX(img_urls) AS img_urls
+                SELECT upso_nm, MAX(img_urls) AS img_urls
                 FROM restaurant_images
-                GROUP BY upso_name
+                GROUP BY upso_nm
             ) A
             JOIN (
-                SELECT upso_name, MAX(url) AS url, MAX(addr) AS addr, MAX(score) AS score
+                SELECT upso_nm, MAX(url) AS url, MAX(addr) AS addr, MAX(score) AS score
                 FROM kakao
-                GROUP BY upso_name
-            ) B ON A.upso_name = B.upso_name;"""
+                GROUP BY upso_nm
+            ) B ON A.upso_nm = B.upso_nm;"""
     
     df = df_load(query)
 
     # 개선된 비교 방식 (유사 일치 허용)
-    matched_df = df[df["upso_name"].str.replace(" ", "").str.lower() == upso.replace(" ", "").lower()]
+    matched_df = df[df["upso_nm"].str.replace(" ", "").str.lower() == upso.replace(" ", "").lower()]
     
     if matched_df.empty:
         return JSONResponse(status_code=404, content={"message": "No matching restaurant found."})
 
+    matched_df = matched_df.copy()
     matched_df["img_url_list"] = matched_df["img_urls"].apply(
         lambda x: [url.strip() for url in x.split(";") if url.strip()]
     )
@@ -199,26 +214,16 @@ async def model_restaurant():
 
     query = """
             WITH ranked AS (
-            SELECT CGG_CODE, ADM_DISPO_YMD, GNT_NO, SNT_COB_NM, SNT_UPTAE_NM, UPSO_NM, SITE_ADDR_RD, DRT_INSP_YMD, ADMM_STATE, DISPO_CTN, BAS_LAW, VIOR_YMD, VIOL_CN, DISPO_CTN_DT, TRDP_AREA,
-                    ROW_NUMBER() OVER (PARTITION BY UPSO_NM ORDER BY ADM_DISPO_YMD DESC) AS rn
+            SELECT CGG_CODE, ADM_DISPO_YMD, GNT_NO, SNT_COB_NM, SNT_UPTAE_NM, upso_nm, SITE_ADDR_RD, DRT_INSP_YMD, ADMM_STATE, DISPO_CTN, BAS_LAW, VIOR_YMD, VIOL_CN, DISPO_CTN_DT, TRDP_AREA,
+                    ROW_NUMBER() OVER (PARTITION BY upso_nm ORDER BY ADM_DISPO_YMD DESC) AS rn
             FROM tb_restaurant_hygiene
-            WHERE SNT_UPTAE_NM IN (
-                '한식', '김밥(도시락)', '탕류(보신용)', '경양식', '호프/통닭',
-                '분식', '단란주점', '일식', '기타 휴게음식점', '간이주점',
-                '커피숍', '일반조리판매', '중국식', '식육(숯불구이)',
-                '외국음식전문점(인도,태국등)', '제과점영업', '정종/대포집/소주방',
-                '감성주점', '패밀리레스트랑', '라이브카페', '키즈카페',
-                '냉면집', '떡카페', '뷔페식', '공동탕업', '통닭(치킨)',
-                '까페', '패스트푸드', '스텐드바', '비어(바)살롱',
-                '전통찻집', '과자점', '도시락제조업', '푸드트럭'
             )
-            )
-            SELECT CGG_CODE, ADM_DISPO_YMD, GNT_NO, SNT_COB_NM, SNT_UPTAE_NM, UPSO_NM, SITE_ADDR_RD, DRT_INSP_YMD, ADMM_STATE, DISPO_CTN, BAS_LAW, VIOR_YMD, VIOL_CN, DISPO_CTN_DT, TRDP_AREA
+            SELECT CGG_CODE, ADM_DISPO_YMD, GNT_NO, SNT_COB_NM, SNT_UPTAE_NM, upso_nm, SITE_ADDR_RD, DRT_INSP_YMD, ADMM_STATE, DISPO_CTN, BAS_LAW, VIOR_YMD, VIOL_CN, DISPO_CTN_DT, TRDP_AREA
             FROM ranked
             WHERE rn = 1
             order by ADM_DISPO_YMD desc
-
-            LIMIT 1000;
+            limit 3000
+            ;
             """
     df = df_load(query)
 
@@ -235,7 +240,7 @@ async def model_restaurant(upso_nm: str = Path(..., title="업소명")):
     query = f"""
         SELECT *
         FROM restaurant_hygiene.tb_restaurant_hygiene
-        WHERE UPSO_NM = '{upso_nm}'
+        WHERE upso_nm = '{upso_nm}'
     """
     df = df_load(query)
     
@@ -244,8 +249,6 @@ async def model_restaurant(upso_nm: str = Path(..., title="업소명")):
     
     # DataFrame을 딕셔너리 리스트 형식으로 변환
     return df.to_dict(orient='records')
-
-
 
 
 
@@ -309,7 +312,7 @@ async def login_api(
         )
 
     # 토큰 생성
-    access_token_expires = timedelta(minutes=30)
+    access_token_expires = timedelta(minutes=600)
     access_token = create_access_token(data={"sub": id}, expires_delta=access_token_expires)
 
     # 로그인 성공 시 토큰과 함께 사용자 ID 전달
@@ -319,4 +322,153 @@ async def login_api(
 # 보호된 API 예시
 @app.get("/api/protected")
 async def protected_route(current_user: str = Depends(verify_token)):
-    return {"message": f"Hello {current_user}, you have access to this protected route."}
+
+    query = f"select id, name, address from user where id = '{current_user}'"
+    df = df_load(query)
+    user_data = df.iloc[0].to_dict()
+    return {"user" : user_data}
+
+
+
+@app.post("/api/favorites")
+async def add_favorite(upso_nm: dict, id: str = Depends(verify_token)):
+    df = pd.DataFrame({
+        "upso_nm": [upso_nm["upso_nm"]],  # 리스트로 감싸야 1개의 행 생성
+        "id"     : [id]
+    })
+    # print(df)
+    df_save(df, "favorites")  # 테이블 이름 명확히!
+    return {"message": "즐겨찾기 추가 완료"}
+
+@app.delete("/api/favorites/{upso_nm}")
+async def delete_favorite(upso_nm: str, id: str = Depends(verify_token)):
+    query = f"DELETE FROM favorites WHERE id = '{id}' AND upso_nm = '{upso_nm}'"
+    delete_data(query)
+    return {"message": "즐겨찾기 삭제 완료"}
+
+
+
+
+
+@app.get("/api/favorites_search")
+async def get_favorites(
+    current_user: str = Depends(verify_token)
+):
+    # current_user가 id라고 가정
+    query = f'''SELECT A.id, A.upso_nm, B.SNT_UPTAE_NM ,B.MAIN_EDF ,B.SITE_ADDR_RD
+                FROM favorites A join model_restaurant_apply B on A.upso_nm = B.upso_nm
+                WHERE id  = "{current_user}"'''
+    df = df_load(query)
+
+    # 이미지 정보 조회
+    query2 = "SELECT * FROM restaurant_hygiene.restaurant_images;"
+    df_photo = df_load(query2)
+
+    # 이미지 이름 정제 및 추출
+    df_photo["img_url"] = df_photo["img_urls"].apply(lambda x: x.split(";")[0].strip())
+
+    # 음식점 이름 정제 후 조인 기준 열 생성
+    df["normalized_upso"] = df["upso_nm"].str.replace(" ", "").str.lower()
+    df_photo["normalized_upso"] = df_photo["upso_nm"].str.replace(" ", "").str.lower()
+
+    # 이미지 URL 병합
+    df = df.merge(df_photo[["normalized_upso", "img_url"]], how="left", on="normalized_upso")
+
+    # 주소 필터용 행정동 추출
+    df["addr"] = df["SITE_ADDR_RD"].apply(lambda x: x.split(" ")[1] if isinstance(x, str) and len(x.split(" ")) > 1 else "")
+    
+    return JSONResponse(content=df.to_dict(orient="records"))
+
+
+
+
+
+
+@app.get("/api/recommend_score")
+async def model_restaurant_recommend_score():
+    # 음식점 정보 조회
+    query = """
+        SELECT 
+            ASGN_YMD,
+            upso_nm,
+            SITE_ADDR_RD,
+            SNT_UPTAE_NM,
+            MAIN_EDF
+        FROM model_restaurant_apply
+        WHERE ASGN_YMD != '' AND ASGN_CANCEL_YMD = ''
+        ORDER BY ASGN_YMD DESC;
+    """
+    df = df_load(query)
+
+    # 이미지 정보 조회
+    query2 = "SELECT A.upso_nm, A.img_urls, B.score, A.addr FROM restaurant_hygiene.restaurant_images A join kakao B on A.id = B.id where (score >= 4.0);"
+    df_photo = df_load(query2)
+    # print(df_photo)
+    # 이미지 이름 정제 및 추출
+    df_photo["img_url"] = df_photo["img_urls"].apply(lambda x: x.split(";")[0].strip())
+
+    # 음식점 이름 정제 후 조인 기준 열 생성
+    df["normalized_upso"] = df["upso_nm"].str.replace(" ", "").str.lower()
+    df_photo["normalized_upso"] = df_photo["upso_nm"].str.replace(" ", "").str.lower()
+
+    df_photo['SITE_ADDR_RD'] = df_photo['addr']
+    df['SITE_ADDR_RD'] = df['SITE_ADDR_RD'].apply(lambda x: x.split(",")[0])
+    # 이미지 URL 병합
+    df = df.merge(
+    df_photo[["normalized_upso", "SITE_ADDR_RD", "img_url", "score"]],
+    how="left",
+    on=["normalized_upso", "SITE_ADDR_RD"]
+)
+
+    # 주소 필터용 행정동 추출
+    df["addr"] = df["SITE_ADDR_RD"].apply(lambda x: x.split(" ")[1] if isinstance(x, str) and len(x.split(" ")) > 1 else "")
+
+    
+    df = df[df["score"].notna()]
+    # 응답 형식 정리   
+    response = df[[
+        "upso_nm",
+        "SITE_ADDR_RD",
+        "SNT_UPTAE_NM",
+        "ASGN_YMD",
+        "img_url",
+        "addr",
+        "MAIN_EDF",
+        "score"
+    ]].fillna("").to_dict(orient="records")
+    return JSONResponse(content=response)
+
+
+
+
+
+
+@app.get("/api/analysis")
+async def analysis():
+    # 1) 모범음식점 집계
+    query1 = """
+    SELECT
+      SUBSTRING_INDEX(SUBSTRING_INDEX(SITE_ADDR_RD, ' ', 2), ' ',-1) AS gu,
+      COUNT(*) AS cnt
+    FROM model_restaurant_apply
+    WHERE SITE_ADDR_RD LIKE '%%구%%'
+      AND ASGN_YMD != '' AND ASGN_CANCEL_YMD = ''
+    GROUP BY gu;
+    """
+    df_model = df_load(query1)
+
+    # 2) 행정처분 업소 집계
+    query2 = """
+    SELECT
+      SUBSTRING_INDEX(SUBSTRING_INDEX(SITE_ADDR_RD, ' ', 2), ' ',-1) AS gu,
+      COUNT(*) AS cnt
+    FROM tb_restaurant_hygiene
+    WHERE SITE_ADDR_RD LIKE '%%구%%'
+    GROUP BY gu;
+    """
+    df_dispo = df_load(query2)
+
+    return JSONResponse({
+      "model_count": df_model.to_dict(orient="records"),
+      "disposition_count": df_dispo.to_dict(orient="records"),
+    })
